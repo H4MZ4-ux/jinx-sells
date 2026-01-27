fffe40e605a257cc660d6649367e1454297275c2
H4MZ4-ux <hmrulez500@hotmail.com>
2026-01-27 10:55:33 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "./index.css";
-import { CartProvider } from "@/context/CartContext";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
+createRoot(document.getElementById("root")!).render(<App />);
